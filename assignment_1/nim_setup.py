301403fa10a97fd255576a8e8bdd0e13f434466c
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-09-23 17:23:24 -0400

    add more games to try

--- assignment_1/nim_setup.py
+++ assignment_1/nim_setup.py
@@ -16,8 +16,11 @@ class NimGame:
     Here we store each board state as a tuple. Each element of the tuple represent the number of chips
     in the stack. The length of the tuple (n) represents the number of stacks in the Nim Game. 
     """
-    board_1 = (1, 2)
-    board_2 = (5, 12, 13)
-    board_3 = (10, 20, 30, 40, 50)
-    board_4 = (1, 2, 3, 4, 5, 6, 7, 8, 9)
-    board_5 = (5, 5, 5, 5)
+    board_1 = [1, 2]
+    board_2 = [5, 12, 13]
+    board_3 = [10, 20, 30, 40, 50]
+    board_4 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    board_5 = [5, 5, 5, 5]
+
+# here we define a max number of winning moves we want to print. 
+WINNING_MOVE_LIMIT: int = 100
