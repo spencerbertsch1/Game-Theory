6c247ba9a2b9dd10ddf4bd47347ecdcf2df62e7d
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-10-01 15:42:26 -0400

    add a fun bit of code about lambda functions

--- assignment_1/routines.py
+++ assignment_1/routines.py
@@ -19,5 +19,26 @@ def even_if_not_all_all_if_odd():
             g_x = (x+1)/2
         print(f'x={x}, g(x)={g_x}')
 
+
+def print_if_odd(l: list) -> list:
+
+    # METHOD #1: use list comprehension to accomplish this goal 
+    print([x for x in l if x%2 == 1])
+
+    # METHOD #2: use the filter 
+    print(list(filter(lambda x: x%2 == 1, l)))
+
+    # METHOD #3: lazy way 
+    odds = []
+    for element in l:
+        if element%2==1:
+            odds.append(element)
+    print(odds)
+
+
+
+
 if __name__ == "__main__":
-    even_if_not_all_all_if_odd()
+    # even_if_not_all_all_if_odd()
+
+    print_if_odd(l=[1, 2, 3, 4, 5, 6, 7])
