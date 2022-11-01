0663bfc3db8de1f94a0db58ee5689e6b646a5a1a
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-11-01 12:11:34 -0400

    print p and q as well

--- assignment_3/guess_it.py
+++ assignment_3/guess_it.py
@@ -30,9 +30,9 @@ def guess_it(n: int, m: int, VERBOSE: bool = False):
     
     # --- BASE CASE ---
     if ((m > 0) & (n == 0)):
-        return 1
+        return {'v': 1, 'p': [], 'q': []}
     elif ((m == 0) & (n > 0)):
-        return (1/(1+n))
+        return {'v': (1/(1+n)), 'p': [], 'q': []}
     else:
 
         # --- RECURSIVE CASE --- 
@@ -43,17 +43,20 @@ def guess_it(n: int, m: int, VERBOSE: bool = False):
         # here we fill in all the values in the board by making three distinct recursive calls
 
         # Upper Left # 
-        upper_left_g_val = 1 - guess_it(n=n, m=m-1)
+        solution_dict = guess_it(n=n, m=m-1)
+        upper_left_g_val = 1 - solution_dict['v']
         upper_left_val = (1/(n+1)) + ((n/(n+1)) * upper_left_g_val)
         board[0,0] = upper_left_val
 
         # Upper Right # 
-        upper_right_g_val = 1 - guess_it(n=n, m=m-1)
+        solution_dict = guess_it(n=n, m=m-1)
+        upper_right_g_val = 1 - solution_dict['v']
         upper_right_val = ((n/(n+1)) * upper_right_g_val)
         board[0,1] = upper_right_val
 
         # Lower Left # 
-        lower_left_g_val = 1 - guess_it(n=n-1, m=m)
+        solution_dict = guess_it(n=n-1, m=m)
+        lower_left_g_val = 1 - solution_dict['v']
         lower_left_val = ((n/(n+1)) * lower_left_g_val)
         board[1,0] = lower_left_val
 
@@ -66,13 +69,13 @@ def guess_it(n: int, m: int, VERBOSE: bool = False):
         
         # use the simplex method to solve this 2x2 game 
         game_dict = game.method_seven(A=board)
-        v = game_dict['v']
+        v = round(game_dict['v'], 2)
         p = game_dict['p']
         q = game_dict['q']
         if VERBOSE: 
             print(f'RECURSIVE SOLUTION \n Value: {round(v, 3)} \n p: {p} \n q: {q}')
 
-        return round(v, 2)
+        return {'v': v, 'p': p, 'q': q}
 
 
 def test_guess_it():
@@ -81,20 +84,24 @@ def test_guess_it():
     """
     n,m = 1,1
     # get the value of the current game
-    game_val = guess_it(m=m, n=n)
+    game_val_dict = guess_it(m=m, n=n)
+    v = game_val_dict['v']
 
-    print(f'Value of G(n={n},m={m})= {game_val}')
+    print(f'Value of G(n={n},m={m})= {v}')
 
 
 def pretty_print_solution(solution_dict: dict):
     """
     Utility function to help visualize the solution
     """
-    for m_n_tuple, value in solution_dict.items():
+    for m_n_tuple, value_dict in solution_dict.items():
+        v = value_dict['v']
+        p = value_dict['p']
+        q = value_dict['q']
         m = m_n_tuple[0]
         n = m_n_tuple[1]
-        s = '-'*15
-        print(f'{s}  Solution to G(m={m}, n={n}): {value}  {s}')
+        s = '-'*5
+        print(f'{s}  Solution to G(m={m}, n={n}): v={v}, p={p}, q={q}  {s}')
 
 
 def main():
@@ -105,9 +112,9 @@ def main():
     for i in range(1, 7, 1):
         for j in range(1, 7, 1):
             # get the value of the current game
-            game_val = guess_it(m=i, n=j)
+            game_val_dict = guess_it(m=i, n=j)
             m_n_tuple = (i, j)
-            solution[m_n_tuple] = game_val
+            solution[m_n_tuple] = game_val_dict
 
     pretty_print_solution(solution_dict=solution)
 
